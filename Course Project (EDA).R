82afee350dcfb656586d9b21d1a4fcd3422f676e
Maulid Bwabo <bwabomaulid@gmail.com>
2022-02-18 15:56:50 +0300

    commit 2

--- Course Project (EDA).R
+++ Course Project (EDA).R
@@ -58,4 +58,4 @@ lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
-
+getwd()
